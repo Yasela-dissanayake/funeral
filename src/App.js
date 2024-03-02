ee477b47da2e19f0cb6ead4a3a5dfee7a8238f57
YaselaD <85230521+Yasela-dissanayake@users.noreply.github.com>
2024-03-02 13:59:31 +0530

    Revert "feat: basic layouting and routing done"
    This reverts commit e127ce8a9dce7f56136f290419967fa164b93c35.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import logo from "./logo.svg";
 import "./App.css";
-import BaseLayout from "./components/BaseLayout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <BaseLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
-]);
+import Checkbox from "@mui/material/Checkbox";
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Checkbox defaultChecked />
+      </header>
     </div>
   );
 }
