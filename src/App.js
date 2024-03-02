0b979c2e6f66e51826a904ae3e77b787ebde3e7c
Yasela-dissanayake <yasela2014@gmail.com>
2024-03-02 13:25:41 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Checkbox from "@mui/material/Checkbox";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Checkbox defaultChecked />
       </header>
     </div>
   );
